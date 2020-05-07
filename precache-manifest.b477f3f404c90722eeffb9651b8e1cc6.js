26cea484639e27b9851dcabfebba3568681d28b8
samuelshang <291243897@qq.com>
2020-05-07 17:37:55 +0800

    Updates

---
+++ precache-manifest.b477f3f404c90722eeffb9651b8e1cc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b296201467d931f69b7c4e866fda6769",
+    "url": "/hackernews-react/index.html"
+  },
+  {
+    "revision": "783b287b05f597d7c162",
+    "url": "/hackernews-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "24326cfe543f4d210bfe",
+    "url": "/hackernews-react/static/js/2.eac5ff0a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hackernews-react/static/js/2.eac5ff0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "783b287b05f597d7c162",
+    "url": "/hackernews-react/static/js/main.84ad7106.chunk.js"
+  },
+  {
+    "revision": "45e41cbfa7093c8d0e02",
+    "url": "/hackernews-react/static/js/runtime-main.67f71c52.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hackernews-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
